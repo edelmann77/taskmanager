f54b88d4931db4ad9f0f135fe24d071fdef032ae
Mikkel Edelmann Nielsen <mken@kamstrup.com>
2024-10-30 10:56:06 +0100

    Added some styling to taskitem

--- src/app/services/taskservice.ts
+++ src/app/services/taskservice.ts
@@ -8,8 +8,9 @@ import { Injectable } from '@angular/core';
 export class TaskService {
   tasks: Task[] = [
     { createdAt: new Date(), id: '1', status: 'Todo', title: 'Task 1' },
-    { createdAt: new Date(), id: '1', status: 'In Progress', title: 'Task 2' },
-    { createdAt: new Date(), id: '1', status: 'Completed', title: 'Task 3' },
+    { createdAt: new Date(), id: '2', status: 'In Progress', title: 'Task 2' },
+    { createdAt: new Date(), id: '3', status: 'Completed', title: 'Task 3' },
+    { createdAt: new Date(), id: '4', status: 'Todo', title: 'Task 4' },
   ];
 
   tasksByStatus = (status: Status) => {
