bdfc99a5434af7b957f0fad38c44ed45ac7811dd
Mikkel Edelmann Nielsen <mken@kamstrup.com>
2024-10-31 20:12:58 +0100

    Added taskitempanel

---
+++ src/app/components/taskItemPanel/taskitempanel.component.ts
@@ -0,0 +1,37 @@
+import { Component, inject, Input, signal } from '@angular/core';
+import {
+  CdkDropList,
+  CdkDrag,
+  CdkDragDrop,
+  moveItemInArray,
+} from '@angular/cdk/drag-drop';
+import { Status } from '../../../types/status';
+import { TaskService } from '../../services/taskservice';
+import { TaskItemComponent } from '../taskitem/taskitem.component';
+
+@Component({
+  selector: 'task-item-panel',
+  standalone: true,
+  imports: [CdkDropList, CdkDrag, TaskItemComponent],
+  templateUrl: './taskitempanel.component.html',
+})
+export class TaskItemPanelComponent {
+  @Input() columnType: Status = 'Todo';
+
+  taskService = inject(TaskService);
+
+  tasks = signal(() => this.taskService.tasksByStatus(this.columnType));
+
+  drop(event: CdkDragDrop<Status[]>) {
+    if (event.container === event.previousContainer) {
+      moveItemInArray(this.tasks()(), event.previousIndex, event.currentIndex);
+    } else {
+      const moveTo = event.container.data[0];
+      const moveFrom = event.previousContainer.data[0];
+
+      if (this.taskService.canMove(moveFrom, moveTo)) {
+        this.taskService.updateStatus(event.item.data.id, moveTo);
+      }
+    }
+  }
+}
--- src/app/components/taskmanagercolumn/taskmanagercolumn.component.ts
+++ src/app/components/taskmanagercolumn/taskmanagercolumn.component.ts
@@ -2,13 +2,8 @@ import { Component, signal, inject, Input } from '@angular/core';
 import { Status } from '../../../types/status';
 import { TaskManagerColumnHeaderComponent } from '../taskmanagercolumnheader/taskmanagercolumnheader-component';
 import { TaskItemComponent } from '../taskitem/taskitem.component';
-import { TaskService } from '../../services/taskservice';
-import {
-  CdkDragDrop,
-  CdkDropList,
-  CdkDrag,
-  moveItemInArray,
-} from '@angular/cdk/drag-drop';
+import { TaskItemPanelComponent } from '../taskItemPanel/taskitempanel.component';
+import { CdkDropList, CdkDrag } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'task-manager-column',
@@ -18,27 +13,10 @@ import {
     TaskItemComponent,
     CdkDropList,
     CdkDrag,
+    TaskItemPanelComponent,
   ],
   templateUrl: './taskmanagercolumn.component.html',
 })
 export class TaskManagerColumnComponent {
   @Input() columnType: Status = 'Todo' as Status;
-  currentDate = new Date();
-
-  taskService = inject(TaskService);
-
-  tasks = signal(() => this.taskService.tasksByStatus(this.columnType));
-
-  drop(event: CdkDragDrop<Status[]>) {
-    if (event.container === event.previousContainer) {
-      moveItemInArray(this.tasks()(), event.previousIndex, event.currentIndex);
-    } else {
-      const moveTo = event.container.data[0];
-      const moveFrom = event.previousContainer.data[0];
-
-      if (this.taskService.canMove(moveFrom, moveTo)) {
-        this.taskService.updateStatus(event.item.data.id, moveTo);
-      }
-    }
-  }
 }
